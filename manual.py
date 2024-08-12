2325fc66f72ce222298608fb8c2ecc291ba738e9
lrcgf <32637075+lrcgf0@users.noreply.github.com>
2024-08-12 18:34:02 +0900

    Update manual.py

--- manual.py
+++ manual.py
@@ -8,7 +8,7 @@ def board():
 		grid()
 		for j in range(5):
 			t = i*5+j+1
-			if t in checked:
+			if t in marked:
 				print(end = '| ｘ ')
 			elif t < 10:
 				print('| 0%d' % (t), end = ' ')
@@ -16,13 +16,13 @@ def board():
 				print('| %d' % (t), end = ' ')
 		print('|')
 	grid()
-checked = []
+marked = []
 board()
 for turn in range(8):
-	checked.append(int(input('turn %d\nchoose a number\n' % (turn+1))))
+	marked.append(int(input('turn %d\nchoose a number\n' % (turn+1))))
 	while True:
 		num = random.randrange(1, 25)
-		if not num in checked:
-			checked.append(num)
+		if not num in marked:
+			marked.append(num)
 			break
 	board()
