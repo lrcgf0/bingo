9e771920da253c0ac5bff42a1d3029d89f2a6bdf
lrcgf <32637075+lrcgf0@users.noreply.github.com>
2024-08-12 18:36:14 +0900

    Add files via upload

---
+++ rand.py
@@ -0,0 +1,49 @@
+import random
+def grid():
+	for i in range(26):
+		print(end = '-')
+	print()
+def board():
+	for i in range(5):
+		grid()
+		for j in range(5):
+			t = i*5+j+1
+			if t in marked:
+				print(end = '| ｘ ')
+			elif t < 10:
+				print('| 0%d' % (t), end = ' ')
+			else:
+				print('| %d' % (t), end = ' ')
+		print('|')
+	grid()
+marked = []
+bingo = 0
+for turn in range(16):
+	while True:
+		num = random.randrange(1, 25)
+		if not num in marked:
+			marked.append(num)
+			break
+board()
+for i in range(5):
+	row = []
+	for j in range(5):
+		row.append(i*5+j+1)
+	if all(item in marked for item in row):
+		bingo += 1
+for i in range(5):
+	column = []
+	for j in range(5):
+		column.append(i+1+j*5)
+	if all(item in marked for item in column):
+		bingo += 1
+diag1 = [1, 7, 13, 19, 25]
+diag2 = [5, 9, 13, 17, 21]
+if all(item in marked for item in diag1):
+	bingo += 1
+if all(item in marked for item in diag2):
+	bingo += 1
+if bingo == 4:
+	print('win')
+else:
+	print('lose')
